6e3a264e0d83597f1cba40d916022fe069f84bc9
YigitPasha <yigitemir12.ask@gmail.com>
2026-09-22 07:13:54 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace WindowsFormOgretici
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOgretici = new System.Windows.Forms.Button();
+            this.chbOgretici = new System.Windows.Forms.CheckBox();
+            this.cmbOgretici = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnListBox = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStripNedirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOgretici
+            // 
+            this.btnOgretici.Location = new System.Drawing.Point(12, 26);
+            this.btnOgretici.Name = "btnOgretici";
+            this.btnOgretici.Size = new System.Drawing.Size(60, 41);
+            this.btnOgretici.TabIndex = 0;
+            this.btnOgretici.Text = "Buton Nedir?";
+            this.btnOgretici.UseVisualStyleBackColor = true;
+            this.btnOgretici.Click += new System.EventHandler(this.btnOgretici_Click);
+            // 
+            // chbOgretici
+            // 
+            this.chbOgretici.AutoSize = true;
+            this.chbOgretici.Location = new System.Drawing.Point(98, 39);
+            this.chbOgretici.Name = "chbOgretici";
+            this.chbOgretici.Size = new System.Drawing.Size(109, 17);
+            this.chbOgretici.TabIndex = 1;
+            this.chbOgretici.Text = "CheckBox Nedir?";
+            this.chbOgretici.UseVisualStyleBackColor = true;
+            this.chbOgretici.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // cmbOgretici
+            // 
+            this.cmbOgretici.FormattingEnabled = true;
+            this.cmbOgretici.Items.AddRange(new object[] {
+            "Bu bir ComboBox nesnedir.",
+            "Bir liste içerisindeki seçeneklerden seçim yapmayı sağlar."});
+            this.cmbOgretici.Location = new System.Drawing.Point(0, 73);
+            this.cmbOgretici.Name = "cmbOgretici";
+            this.cmbOgretici.Size = new System.Drawing.Size(319, 21);
+            this.cmbOgretici.TabIndex = 2;
+            this.cmbOgretici.SelectedIndexChanged += new System.EventHandler(this.cmbOgretici_SelectedIndexChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(233, 34);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(335, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 118);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bu bir GroupBox nesnesidir. Kontrolleri belirli gruplar içerisinde düzenlemek içi" +
+    "n kullanılır. Formun daha anlaşılır görünmesini sağlar.";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(541, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(429, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Bu bir Label nesnesidir. Form üzerinde kullanıcıya bilgi veya açıklama vermek içi" +
+    "n kullanılır.";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 120);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnListBox
+            // 
+            this.btnListBox.Location = new System.Drawing.Point(33, 120);
+            this.btnListBox.Name = "btnListBox";
+            this.btnListBox.Size = new System.Drawing.Size(75, 36);
+            this.btnListBox.TabIndex = 7;
+            this.btnListBox.Text = "Listbox nedir?";
+            this.btnListBox.UseVisualStyleBackColor = true;
+            this.btnListBox.Click += new System.EventHandler(this.btnListBox_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStripNedirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1472, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStripNedirToolStripMenuItem
+            // 
+            this.menuStripNedirToolStripMenuItem.Name = "menuStripNedirToolStripMenuItem";
+            this.menuStripNedirToolStripMenuItem.Size = new System.Drawing.Size(109, 20);
+            this.menuStripNedirToolStripMenuItem.Text = "MenuStrip nedir?";
+            this.menuStripNedirToolStripMenuItem.Click += new System.EventHandler(this.menuStripNedirToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 221);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(348, 353);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 42);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "PictureBox Nedir?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(1037, 73);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox2.TabIndex = 12;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1472, 668);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnListBox);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.cmbOgretici);
+            this.Controls.Add(this.chbOgretici);
+            this.Controls.Add(this.btnOgretici);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Windows Forms Öğretici Uygulaması";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOgretici;
+        private System.Windows.Forms.CheckBox chbOgretici;
+        private System.Windows.Forms.ComboBox cmbOgretici;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnListBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuStripNedirToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormOgretici
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOgretici_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+            "Bu bir Buton nesnesidir.\n\n" +
+            "Programlarda belirli bir işlemi başlatmak için kullanılır.\n" +
+            "Üzerine tıklandığında Click olayı çalışır.",
+                "Buton",
+                MessageBoxButtons.OK,
+                 MessageBoxIcon.Information
+            );
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+                        MessageBox.Show(
+                "Bu bir CheckBox nesnesidir.\n\n" +
+                "Bir seçeneğin seçilip seçilmediğini belirtmek için kullanılır. " +
+                "Birden fazla CheckBox aynı anda seçilebilir.",
+                "CheckBox",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+
+        private void cmbOgretici_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+    "Bu bir DateTimePicker nesnesidir.\n\n" +
+    "Kullanıcının tarih ve saat seçmesini sağlar.",
+    "DateTimePicker",
+    MessageBoxButtons.OK,
+    MessageBoxIcon.Information
+    );
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+    "Bu bir GroupBox nesnesidir.\n\n" +
+    "Kontrolleri belirli gruplar içerisinde düzenlemek için kullanılır. " +
+    "Formun daha anlaşılır görünmesini sağlar.",
+    "GroupBox",
+    MessageBoxButtons.OK,
+    MessageBoxIcon.Information
+);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnListBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+    "Bu bir ListBox nesnesidir.\n\n" +
+    "Seçenekleri açık bir liste halinde göstermek ve kullanıcının " +
+    "bu seçeneklerden seçim yapmasını sağlamak için kullanılır.",
+    "ListBox",
+    MessageBoxButtons.OK,
+    MessageBoxIcon.Information
+);
+        }
+
+        private void menuStripNedirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+    "Bu bir MenuStrip nesnesidir.\n\n" +
+    "Program içerisinde menü başlıkları ve alt menüler oluşturmak için kullanılır.",
+    "MenuStrip",
+    MessageBoxButtons.OK,
+    MessageBoxIcon.Information
+);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+    "Bu bir PictureBox nesnesidir.\n\n" +
+    "Form üzerinde resim veya görsel göstermek için kullanılır.",
+    "PictureBox",
+    MessageBoxButtons.OK,
+    MessageBoxIcon.Information
+);
+        }
+    }
+}
